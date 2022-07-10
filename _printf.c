2df7b608ee8ae2c9d7dc17b1b814bf7ced85721a
oupa shabangu <oupa.m.shabangu@gmail.com>
2022-07-10 11:47:23 -0700

    colaboration

---
+++ _printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: format string
+ *
+ * Return: number of charectors printed
+ */
+int _print(const char *format, ...)
+{
+
